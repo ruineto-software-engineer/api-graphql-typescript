3664417497c6009b0693f183bc4ff4202047d0e0
Rui <ruineto11@gmail.com>
2023-06-13 12:25:50 -0300

    first commit

---
+++ simple-server.ts
@@ -0,0 +1,36 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    users: [String!]!
+  }
+
+  type Mutation {
+    createUser(name: String!): String!
+  }
+`;
+
+const users: String[] = [];
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      users: () => {
+        return users;
+      },
+    },
+
+    Mutation: {
+      createUser: (parent, args, ctx) => {
+        users.push(args.name);
+
+        return args.name;
+      },
+    },
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`HTTP Server is running on ${url}`);
+});
